f25f84d6b78013884537d286c99ebf7fcd3edb64
Zemko-o <filip3049@gmail.com>
2025-05-29 18:31:51 +0900

    Initial commit

--- app.py
+++ app.py
@@ -195,4 +195,5 @@ def book():
     return "Booking saved successfully!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
